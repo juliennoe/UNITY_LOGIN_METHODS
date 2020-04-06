d4573ccf929048aebfd18ecdc93425b023b3f86c
jeinsei <juliennoe.job@gmail.com>
2020-04-06 18:20:00 +0200

    Initial commit

---
+++ UNITY_LOGIN/Assets/_SCRIPTS/BasicLogin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasicLogin : MonoBehaviour
+{
+    public InputField pseudo;
+    public InputField password;
+
+
+    void Update()
+    {
+        Debug.Log("enter your loggin");
+
+        if (Input.GetKeyDown("return"))
+        {
+            if (pseudo.text == "julien" && password.text == "230190")
+            {
+                Debug.Log("Loggin valide");
+            }
+            else
+            {
+                Debug.Log("Loggin failed");
+            }
+        
+        }
+
+    }
+}
