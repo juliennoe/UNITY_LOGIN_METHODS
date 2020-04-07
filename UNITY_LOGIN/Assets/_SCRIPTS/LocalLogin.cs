3aaed776e9b96370971ee52dd5588c6c67858308
jeinsei <juliennoe.job@gmail.com>
2020-04-07 11:39:31 +0200

    Local Login

--- UNITY_LOGIN/Assets/_SCRIPTS/LocalLogin.cs
+++ UNITY_LOGIN/Assets/_SCRIPTS/LocalLogin.cs
@@ -4,214 +4,111 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using System.Text.RegularExpressions;
+using System.IO;
 
 public class LocalLogin : MonoBehaviour
 {
-
     public GameObject username;
     public GameObject password;
-    public GameObject email;
-    public GameObject confPassword;
 
     private string username_s;
-    private string email_s;
     private string password_s;
-    private string confPassword_s;
-    private string form_s;
-
-    private string[] characters = {
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-    "_", "-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-
-    private bool emailValid = false;
-
+    private string[] lines;
+    private string decryptedPass;
 
     void Update()
     {
-        // recuparation des strings des input field
-        username_s = username.GetComponent<InputField>().text;
-        email_s = email.GetComponent<InputField>().text;
-        password_s = password.GetComponent<InputField>().text;
-        confPassword_s = confPassword.GetComponent<InputField>().text;
-
         // interaction a l'aide de la touche tab pour passer de champ en champ
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             if (username.GetComponent<InputField>().isFocused)
             {
-                email.GetComponent<InputField>().Select();
-            }
-            if (email.GetComponent<InputField>().isFocused)
-            {
                 password.GetComponent<InputField>().Select();
             }
-            if (password.GetComponent<InputField>().isFocused)
-            {
-                confPassword.GetComponent<InputField>().Select();
-            }
-           
+            username_s = username.GetComponent<InputField>().text;
         }
 
         // interaction de la touche entrer pour valider une fois les champs complets
         if (Input.GetKeyDown(KeyCode.Return))
         {
+            username_s = username.GetComponent<InputField>().text;
+            password_s = password.GetComponent<InputField>().text;
 
-            if(username_s != "" &&  email_s != "" && password_s != "" && confPassword_s != "")
+            if (username_s != "" && password_s != "")
             {
-                RegisterButton();
+                LoginButton();
             }
         }
     }
 
-    // fonction permettant de vérifier si les adresses contiennent des caractères valides en début et fin d'adresse mail
-    private void EmailValidation()
+    // fonction permettant de vérifier si les identifiants sont cohérents avec le fichier texte
+    public void LoginButton()  
     {
-        bool startWith = false;
-        bool endWith = false;
+        username_s = username.GetComponent<InputField>().text;
+        password_s = password.GetComponent<InputField>().text;
 
-        for(int i = 0; i< characters.Length; i++)
-        {
-            if (email_s.StartsWith(characters[i]))
-            {
-                startWith = true;
-            }
-        }
-
-        for (int i = 0; i < characters.Length; i++)
-        {
-            if (email_s.EndsWith(characters[i]))
-            {
-                endWith = true;
-            }
-        }
-
-        if(startWith == true && endWith == true)
-        {
-            emailValid = true;
-        }
-        else
-        {
-            emailValid = false;
-            Debug.LogWarning("vérifier le début et la fin de votre adresse email");
-        }
-    }
-
-    // fonction permettant d'enregistrer les informations des différents champs
-    public void RegisterButton()
-    {
-        // Boolean permettant de valider la qualité des données avant de les enregistrer
         bool username_b = false;
-        bool email_b = false;
         bool password_b = false;
-        bool confPassword_b  = false;
 
-        if (username_s != "")
+        // si le champ username n'est pas vide alors...
+        if(username_s != "")
         {
-            if (!System.IO.File.Exists(@"/Users/noe/Documents/_PROJETS_UNITY/UNITY_VR_TOOLS/UNITY_LOGIN/Assets/" + username_s + ".txt"))
+            if (System.IO.File.Exists(@"/Users/noe/Documents/_PROJETS_UNITY/UNITY_LOGIN_METHODS/UNITY_LOGIN/Assets/"+ username_s +".txt"))
             {
                 username_b = true;
-            }
-            else 
-            {
-                Debug.LogWarning("Ce nom est déjà pris !");
-            }
-        }
-        else
-        {
-            Debug.LogWarning("Le champ nom est vide !");
-        }
-
-        // si le champ email n'est pas vide alors...
-        if (email_s != "")
-        {
-            EmailValidation();
-
-            if (emailValid == true)
-            {
-                if (email_s.Contains("@") && email_s.Contains("."))
-                {
-                    email_b = true;
-                }
-                else
-                {
-                    Debug.LogWarning("Votre email semble incorrect !");
-                }
-            }
-        }
-        else
-        {
-            Debug.LogWarning("Le champ Email semble vide !");
-        }
-
-        // si le champ password email n'est pas vide alors...
-        if (password_s != "")
-            {
-                if(password_s.Length > 5)
-                {
-                password_b = true;
-                }
-                else
-                {
-                Debug.LogWarning("Votre mot de passe doit comprendre 6 caractères minium !");
-                }
-        }
-        else
-        {
-            Debug.LogWarning("Le champ mot de passe semble vide !");
-        }
-
-        // si le champ confPassword n'est pas vide alors...
-        if (confPassword_s != "")
-        {
-            if(confPassword_s  == password_s)
-            {
-                confPassword_b = true;
+                lines = System.IO.File.ReadAllLines(@"/Users/noe/Documents/_PROJETS_UNITY/UNITY_LOGIN_METHODS/UNITY_LOGIN/Assets/" + username_s + ".txt");
             }
             else
             {
-                Debug.LogWarning("Les mots de passes de correspondent pas !");
+                Debug.LogWarning("Le nom semble invalide !");
             }
-        } 
+        }
         else
         {
-            Debug.LogWarning("Le champ confirmer mot de passe semble vide !");
+            Debug.LogWarning("le champ nom semble vide !");
         }
 
-        // si les champs sont correctes alors les booleans passent à true alors...
-        if (username_b == true && password_b == true && email_b == true && confPassword_b == true)
+        // si le champ password n'est pas vide alors...
+        if(password_s != "")
         {
-            bool Clear = true;
-            int i = 1;
-
-            // encodage du passe word dans le document texte
-            foreach(char c in password_s)
+ 
+            if (System.IO.File.Exists(@"/Users/noe/Documents/_PROJETS_UNITY/UNITY_LOGIN_METHODS/UNITY_LOGIN/Assets/" + username_s +".txt"))
             {
-                if(Clear == true)
+                int i = 1;
+
+                // encodage du passe word dans le document texte
+                foreach (char c in lines[2])
                 {
-                    password_s = "";
-                    Clear = false;
+                    i++;
+                    char Decrypted = (char)(c / i);
+                    decryptedPass += Decrypted.ToString();
                 }
-                i++;
-                char Encrypted = (char)(c * i);
-                password_s += Encrypted.ToString();
+            }
+            else
+            {
+                Debug.LogWarning("Le mot de passe n'est pas valide !");
             }
 
-            // enregistrement des données dans le style username > email > password
-            form_s = (username_s + "/" + email_s + "/" + password_s);
+            if (password_s == decryptedPass)
+            {
+                password_b = true;
+            }
+            else
+            {
+                Debug.LogWarning("Le mot de passe n'est pas valide !");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Le champ de mot de passe semble vide !");
+        }
 
-            // stockage des informations dans le fichier texte via le chemin indiqué, sous le nom de l'utilisateur via username et sous la forme form_s
-            System.IO.File.WriteAllText(@"/Users/noe/Documents/_PROJETS_UNITY/UNITY_VR_TOOLS/UNITY_LOGIN/Assets/" + username_s + ".txt", form_s);
-          
-            // reset des différents champs après enregistrement
+        // si les deux champs sont valide alors...
+        if(username_b == true && password_b == true)
+        {
+            Debug.Log("Succes login / open start menu");
             username.GetComponent<InputField>().text = "";
-            email.GetComponent<InputField>().text = "";
             password.GetComponent<InputField>().text = "";
-            confPassword.GetComponent<InputField>().text = "";
-
-            Debug.Log("Registration Complete");
         }
     }
-
-
 }
---
+++ UNITY_LOGIN/Assets/_SCRIPTS/LocalRegister.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+public class LocalRegister : MonoBehaviour
+{
+
+    public GameObject username;
+    public GameObject password;
+    public GameObject email;
+    public GameObject confPassword;
+
+    private string username_s;
+    private string email_s;
+    private string password_s;
+    private string confPassword_s;
+    private string form_s;
+
+    private string[] characters = {
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "_", "-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+    private bool emailValid = false;
+
+
+    void Update()
+    {
+        // recuparation des strings des input field
+        username_s = username.GetComponent<InputField>().text;
+        email_s = email.GetComponent<InputField>().text;
+        password_s = password.GetComponent<InputField>().text;
+        confPassword_s = confPassword.GetComponent<InputField>().text;
+
+        // interaction a l'aide de la touche tab pour passer de champ en champ
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (username.GetComponent<InputField>().isFocused)
+            {
+                email.GetComponent<InputField>().Select();
+            }
+            if (email.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+            if (password.GetComponent<InputField>().isFocused)
+            {
+                confPassword.GetComponent<InputField>().Select();
+            }
+           
+        }
+
+        // interaction de la touche entrer pour valider une fois les champs complets
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+
+            if(username_s != "" &&  email_s != "" && password_s != "" && confPassword_s != "")
+            {
+                RegisterButton();
+            }
+        }
+    }
+
+    // fonction permettant de vérifier si les adresses contiennent des caractères valides en début et fin d'adresse mail
+    private void EmailValidation()
+    {
+        bool startWith = false;
+        bool endWith = false;
+
+        for(int i = 0; i< characters.Length; i++)
+        {
+            if (email_s.StartsWith(characters[i]))
+            {
+                startWith = true;
+            }
+        }
+
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if (email_s.EndsWith(characters[i]))
+            {
+                endWith = true;
+            }
+        }
+
+        if(startWith == true && endWith == true)
+        {
+            emailValid = true;
+        }
+        else
+        {
+            emailValid = false;
+            Debug.LogWarning("vérifier le début et la fin de votre adresse email");
+        }
+    }
+
+    // fonction permettant d'enregistrer les informations des différents champs
+    public void RegisterButton()
+    {
+        // Boolean permettant de valider la qualité des données avant de les enregistrer
+        bool username_b = false;
+        bool email_b = false;
+        bool password_b = false;
+        bool confPassword_b  = false;
+
+        // si le champ username n'est pas vide alors...
+        if (username_s != "")
+        {
+            if (!System.IO.File.Exists(@"/Users/noe/Documents/_PROJETS_UNITY/UNITY_LOGIN_METHODS/UNITY_LOGIN/Assets/" + username_s + ".txt"))
+            {
+                username_b = true;
+            }
+            else 
+            {
+                Debug.LogWarning("Ce nom est déjà pris !");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Le champ nom est vide !");
+        }
+
+        // si le champ email n'est pas vide alors...
+        if (email_s != "")
+        {
+            EmailValidation();
+
+            if (emailValid == true)
+            {
+                if (email_s.Contains("@") && email_s.Contains("."))
+                {
+                    email_b = true;
+                }
+                else
+                {
+                    Debug.LogWarning("Votre email semble incorrect !");
+                }
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Le champ Email semble vide !");
+        }
+
+        // si le champ password email n'est pas vide alors...
+        if (password_s != "")
+            {
+                if(password_s.Length > 5)
+                {
+                password_b = true;
+                }
+                else
+                {
+                Debug.LogWarning("Votre mot de passe doit comprendre 6 caractères minium !");
+                }
+        }
+        else
+        {
+            Debug.LogWarning("Le champ mot de passe semble vide !");
+        }
+
+        // si le champ confPassword n'est pas vide alors...
+        if (confPassword_s != "")
+        {
+            if(confPassword_s  == password_s)
+            {
+                confPassword_b = true;
+            }
+            else
+            {
+                Debug.LogWarning("Les mots de passes de correspondent pas !");
+            }
+        } 
+        else
+        {
+            Debug.LogWarning("Le champ confirmer mot de passe semble vide !");
+        }
+
+        // si les champs sont correctes alors les booleans passent à true alors...
+        if (username_b == true && password_b == true && email_b == true && confPassword_b == true)
+        {
+            bool Clear = true;
+            int i = 1;
+
+            // encodage du passe word dans le document texte
+            foreach(char c in password_s)
+            {
+                if(Clear == true)
+                {
+                    password_s = "";
+                    Clear = false;
+                }
+                i++;
+                char Encrypted = (char)(c * i);
+                password_s += Encrypted.ToString();
+            }
+
+            // enregistrement des données dans le style username > email > password
+            form_s = (username_s + Environment.NewLine + email_s + Environment.NewLine + password_s);
+
+            // stockage des informations dans le fichier texte via le chemin indiqué, sous le nom de l'utilisateur via username et sous la forme form_s
+            System.IO.File.WriteAllText(@"/Users/noe/Documents/_PROJETS_UNITY/UNITY_LOGIN_METHODS/UNITY_LOGIN/Assets/" + username_s + ".txt", form_s);
+          
+            // reset des différents champs après enregistrement
+            username.GetComponent<InputField>().text = "";
+            email.GetComponent<InputField>().text = "";
+            password.GetComponent<InputField>().text = "";
+            confPassword.GetComponent<InputField>().text = "";
+
+            Debug.Log("Registration Complete");
+        }
+    }
+
+
+}
